74f2bc531a1e6df3f81f4448ad00e93d1c0f7423
Robert Iagar <robert.iagar@gmail.com>
2015-05-20 15:55:04 +0300

    Adding Windows Store clients to project.

---
+++ Sewn.Clients/Sewn.Clients.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Phone.PersonalInformation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Sewn.Clients
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+
+            var store = await ContactStore.CreateOrOpenAsync(ContactStoreSystemAccessMode.ReadWrite, ContactStoreApplicationAccessMode.ReadOnly);
+            var phoneStore = await Windows.ApplicationModel.Contacts.ContactManager.RequestStoreAsync();
+            var contacts = await phoneStore.FindContactsAsync("+40745901401");
+            var contact = new StoredContact(store);
+            contact.RemoteId = "4343";
+            contact.GivenName = "test";
+            contact.FamilyName = "succes 2";
+            var props = await contact.GetPropertiesAsync();
+
+
+            props.Add(KnownContactProperties.WorkEmail, "work@mail.com");
+            props.Add(KnownContactProperties.MobileTelephone, "+40745901401");
+
+            await contact.SaveAsync();
+        }
+    }
+}
