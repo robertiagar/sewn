9f4ecc0171aa2b9bc149f09c1a074ca6924dabf9
Robert Iagar <robert.iagar@gmail.com>
2015-05-21 16:32:52 +0300

    Adding email service.
    Using google smtp, but may be ineffective. (99emails/day).

---
+++ Sewn/Services/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Sewn.Services
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("myusername@gmail.com", "mypwd"),
+                EnableSsl = true
+            };
+            await client.SendMailAsync("from@email.com", message.Destination, message.Subject, message.Body);
+            Console.WriteLine("Sent");
+            Console.ReadLine();
+        }
+    }
+}
